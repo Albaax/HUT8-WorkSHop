1c2a05174d861d0f1396d490046dad28fd575f1e
Alberto Helbig <albertohelbig@gmail.com>
2021-07-31 16:25:12 -0300

    Final queries updated

--- src/Components/Options/index.js
+++ src/Components/Options/index.js
@@ -3,12 +3,10 @@ import { NavLink } from "react-router-dom";
 
 const Options = () => {
 
-
-
     return (
             <ul className="options">
                 <li><NavLink exact className="link" activeClassName="link-active" to="/">Dashboard</NavLink></li>
-                <li><NavLink  className="link" activeClassName="link-active" to="/mycourses">Meus cursos</NavLink></li>
+                <li><NavLink className="link" activeClassName="link-active" to="/mycourses">Meus cursos</NavLink></li>
             </ul>
     )
 }
